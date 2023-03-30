bab72f5ab897d26bc36ef74f8ef90df3dd1e3cdf
Merunj <93353746+Merunj@users.noreply.github.com>
2023-03-31 01:32:38 +0300

    Update departament.py

--- data/departament.py
+++ data/departament.py
@@ -9,7 +9,7 @@ class Departament(SqlAlchemyBase):
     id = sqlalchemy.Column(sqlalchemy.Integer, 
                            primary_key=True, autoincrement=True)
     title = sqlalchemy.Column(sqlalchemy.String, nullable=True)
-    chief = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    chief = sqlalchemy.Column(sqlalchemy.Integer, autoincrement=True)
     email = sqlalchemy.Column(sqlalchemy.String, 
                               index=True, unique=True, nullable=True)
     member = sqlalchemy.Column(sqlalchemy.Integer, 
